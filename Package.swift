929d53438e665bdd0a6eaec02be801cdee4da3e4
Alexandru Astilean <alexandru.astilean@gmail.com>
2022-10-22 02:28:39 +0300

    Fixed SPM support.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.6
+
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+  name: "Locksmith",
+	products: [
+		.library(name: "Locksmith", targets: ["Locksmith"])
+	],
+	targets: [
+		.target(name: "Locksmith", path: "Source")
+	]
 )
